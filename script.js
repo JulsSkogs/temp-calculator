c497fecda5317814156434a087b467748eac007f
JulsSkogs <rjsollenskog@gmail.com>
2023-01-28 02:10:23 -0500

    implement conversion function and add mroe event listeners

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const calculatedTemp = document.getElementById("output");
+calculatedTemp.textContent = "";
 const inF = document.getElementById("inF");
 const inC = document.getElementById("inC");
 const inK = document.getElementById("inK");
@@ -7,12 +8,13 @@ const outC = document.getElementById("outC");
 const outK = document.getElementById("outK");
 const calcBtn = document.getElementById("calc-btn");
 let temp = document.getElementById("input");
-let inputUnit;
-let outputUnit;
+
+// let inUnit;
+// let outUnit;
 
 // F to C: ((F - 32) * .5556) = C
-// C to F: ((C * 1.8) + 32) = F
 // F to K: ((F + 459.67) * (5/9) = K
+// C to F: ((C * 1.8) + 32) = F
 // C to K: C + 273.15 = K
 // K to F: (K * (9/5) - 459.67) = F
 // K to C: K - 273.15 = C
@@ -21,34 +23,68 @@ let outputUnit;
 // https://en.wikipedia.org/wiki/Rankine_scale
 // https://www.almanac.com/predict-temperature-cricket-chirps
 
-function fToC(temp) {
-  let C = (temp - 32) * 0.5556;
-  return C;
+// Conversion function
+function convert(temp, inUnit, outUnit) {
+  if (inUnit === "F" && outUnit === "C") {
+    conTemp = (temp - 32) * 0.5556;
+    return conTemp;
+  } else if (inUnit === "F" && outUnit === "K") {
+    conTemp = (temp + 459.67) * (5 / 9);
+    return conTemp;
+  } else if (inUnit === "C" && outUnit === "F") {
+    conTemp = temp * 1.8 + 32;
+    return conTemp;
+  } else if (inUnit === "C" && outUnit === "K") {
+    conTemp = temp + 273.15;
+    return conTemp;
+  } else if (inUnit === "K" && outUnit === "F") {
+    conTemp = temp * (9 / 5) - 459.67;
+    return conTemp;
+  } else if (inUnit === "K" && outUnit === "F") {
+    conTemp = temp - 273.15;
+    return conTemp;
+  }
+  calculatedTemp.textContent = `${conTemp}`;
 }
-
-function cToF(temp) {
-  let F = temp * 1.8 + 32;
-  return F;
-}
-
-function fToK(temp) {
-  let K = (temp + 459.67) * (5 / 9);
-  return K;
-}
-
-function cToK(temp) {
-  let K = temp + 273.15;
-  return K;
-}
-
-function kToF(temp) {
-  let F = temp * (9 / 5) - 459.67;
-  return F;
-}
-
-function kToC(temp) {
-  let C = temp - 273.15;
-  return C;
-}
+// console.log(convert(0, 'C', 'F'));
 
 // Event Listeners
+inF.addEventListener("click", function () {
+  inC.disabled;
+  inK.disabled;
+  inUnit = "F";
+});
+
+inC.addEventListener("click", function () {
+  inF.disabled;
+  inK.disabled;
+  inUnit = "C";
+  console.log(inUnit);
+});
+
+inK.addEventListener("click", function () {
+  inF.disabled;
+  inC.disabled;
+  inUnit = "K";
+});
+
+outF.addEventListener("click", function () {
+  outC.disabled;
+  outK.disabled;
+  outUnit = "F";
+});
+
+outC.addEventListener("click", function () {
+  outF.disabled;
+  outK.disabled;
+  outUnit = "C";
+});
+
+outK.addEventListener("click", function () {
+  outF.classList.add("hidden");
+  outF.disabled;
+  outC.disabled;
+  outUnit = "K";
+});
+
+calcBtn.addEventListener("click", convert(temp));
